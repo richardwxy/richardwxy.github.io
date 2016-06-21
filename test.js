be92c7528715c034173158e2e1a6ee06552fb3bb
richardwxy <fandongyu356@gamil.com>
2016-06-21 13:36:44 +0800

    test

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ defineClass("JSViewController", {
   //instance method definitions
   viewDidLoad: function() {
     require('NSArray')
-    self.array = NSArray.alloc().initWithObjects("123","titi", nil);
+    self.array = NSArray.alloc().initWithObjects("123","titi");
     var alertView = require('UIAlertView').alloc().init();
  	alertView.setTitle('提示');
  	alertView.setMessage(array.objectAtIndex(1)); 
