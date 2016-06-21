28aa9efe8ee1544d83086eefbd2078975e4b02ac
richardwxy <fandongyu356@gamil.com>
2016-06-21 13:40:14 +0800

    test

--- test.js
+++ test.js
@@ -1,15 +1,11 @@
 defineClass("JSViewController", {
   //instance method definitions
   viewDidLoad: function() {
-    require('NSArray')
-    self.array = NSArray.alloc().initWithObjects("123","titi");
-    var alertView = require('UIAlertView').alloc().init();
- 	alertView.setTitle('提示');
- 	alertView.setMessage(array.objectAtIndex(1)); 
- 	alertView.addButtonWithTitle('OK');
- 	alertView.show(); 
-
-
+var alertView = require('UIAlertView').alloc().init();
+     alertView.setTitle('Alert');
+     alertView.setMessage('AlertView from js'); 
+     alertView.addButtonWithTitle('OK');
+     alertView.show(); 
     }
   }
 })
