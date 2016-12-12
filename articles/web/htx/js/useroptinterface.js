de75cd690b2bb4a4c0b965fcd5b1325eaa092364
richardwxy <fandongyu356@gamil.com>
2016-12-12 18:46:55 +0800

    update

--- articles/web/htx/js/useroptinterface.js
+++ articles/web/htx/js/useroptinterface.js
@@ -188,9 +188,9 @@ case "2":
 }
 
 function userOptgpbysem(nameBox, mobileBox,codeBtn,opType,tips) {
-	if(typeof(tips) == "undefined"){
+if(typeof(tips) == "undefined"){
 		tips="您已操作成功！客服人员会电话联系您，请稍作等待！";
-	}
+		}
 	var name=$.trim($("[name='"+nameBox+"']").val());
 	var mobile = $.trim($("[name='"+mobileBox+"']").val());
 
@@ -219,20 +219,8 @@ function userOptgpbysem(nameBox, mobileBox,codeBtn,opType,tips) {
 		layer.close(loadi);
 		switch (data.Code){
 		case 0:
-		    alert('您已操作成功！客服人员会电话联系您，请稍作等待！');
-			function getQueryString(name) {
-				var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-				var r = window.location.search.substr(1).match(reg);
-				if (r != null) return unescape(r[2]); return null;
-			}
-			var token = getQueryString("token");
-			var wecha_source_id = getQueryString("wecha_source_id");
-			var id = getQueryString("id");
-			var wmarkId = getQueryString("wmarkId");
-
-			window.location.href="http://ali.ichaotu.com/index.php?g=Wap&m=Huitx&a=index&token="+token+"&wecha_source_id="+wecha_source_id+"&id="+id+"&wmarkId="+wmarkId;
-
-		  //location.reload();
+		  alert('您已操作成功！客服人员会电话联系您，请稍作等待！');
+		  location.reload();
 		  break;
 		case "1":
 		  layer.tips("手机号码格式错误！", $("[name='"+mobileBox+"']"), { guide: 2, time: 3 });
